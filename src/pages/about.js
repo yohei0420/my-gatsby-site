ee2bb686ce263e00a3e7982526cf49ee1c7cf2fa
yohei <si18024@edu.aomori-u.ac.jp>
2021-10-30 22:36:02 +0900

    ページ追加

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,12 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import {Link} from 'gatsby'
 const AboutPage = () => {
   return (
     <Layout pageTitle="About Me">
       <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+      <Link to="/nebuta"><p>ねぶた</p></Link>
+      <Link to="/sansya"><p>三社</p></Link>
     </Layout>
   )
 }
---
+++ src/pages/sansya.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const Sansya = () => {
+  return (
+    <Layout pageTitle="八戸三社大祭">
+      <p>八戸三社大祭です</p>
+      <h2>始まり</h2>
+        <p>享保5年（1720）、凶作に悩む八戸の有力者たちが、法霊大明神（現在のおがみ神社）に天候の回復と豊作を祈願したところ、無事に秋の収穫を迎えることができました。その御礼として、八戸藩の許可のもと、武士や町人から寄進を募って神輿を建造し、享保6年（1721）に長者山三社堂（現在の新羅神社）に渡御したことが、八戸三社大祭の始まりと言われています。</p>
+        <hr/>
+    </Layout>
+  )
+}
+export default Sansya
