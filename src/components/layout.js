f38bd08d5357ab6f037d7520552f7b0f578b3752
yohei <si18024@edu.aomori-u.ac.jp>
2021-11-03 21:50:43 +0900

    ナビゲーション追加

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,11 +3,10 @@ import { Link, useStaticQuery, graphql } from 'gatsby'
 import {
   container,
   heading,
-  navLinks,
-  navLinkItem,
-  navLinkText,
   siteTitle,
 } from './layout.module.css'
+import { Container, Navbar, Nav } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 const Layout = ({ pageTitle, children }) => {
   const data = useStaticQuery(graphql`
     query {
@@ -21,26 +20,26 @@ const Layout = ({ pageTitle, children }) => {
   return (
     <div className={container}>
       <title>{pageTitle} | {data.site.siteMetadata.title}</title>
-      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
-      <nav>
-        <ul className={navLinks}>
-          <li className={navLinkItem}>
-            <Link to="/" className={navLinkText}>
-              Home
-            </Link>
-          </li>
-          <li className={navLinkItem}>
-            <Link to="/about" className={navLinkText}>
-              About
-            </Link>
-          </li>
-          <li className={navLinkItem}>
-            <Link to="/blog" className={navLinkText}>
-              Blog
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      <header className="bg-dark">
+      <Container>
+    <Navbar expand="md" variant="dark">
+      <Navbar.Toggle aria-controls="navbarResponsive" />
+      <Navbar.Collapse id="navbarResponsive">
+        <Nav as="ul" className="ml-auto">
+        <Nav.Item as="li">
+            <Link to="/" className="nav-link" activeClassName="active">Home</Link>
+          </Nav.Item>
+          <Nav.Item as="li">
+            <Link to="/about" className="nav-link" activeClassName="active">About</Link>
+          </Nav.Item>
+          <Nav.Item as="li">
+            <Link to="/blog" className="nav-link" activeClassName="active">Blog</Link>
+          </Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  </Container> 
+  </header>
       <main>
         <h1 className={heading}>{pageTitle}</h1>
         {children}
--- src/pages/sansya.js
+++ src/pages/sansya.js
@@ -1,13 +1,17 @@
 import * as React from 'react'
 import Layout from '../components/layout'
-
+import Button from "react-bootstrap/Button";
+import { Link as Scroll } from 'react-scroll';
 const Sansya = () => {
   return (
     <Layout pageTitle="八戸三社大祭">
-      <p>八戸三社大祭です</p>
+      <section id="concept"><p>八戸三社大祭です</p></section>
       <h2>始まり</h2>
         <p>享保5年（1720）、凶作に悩む八戸の有力者たちが、法霊大明神（現在のおがみ神社）に天候の回復と豊作を祈願したところ、無事に秋の収穫を迎えることができました。その御礼として、八戸藩の許可のもと、武士や町人から寄進を募って神輿を建造し、享保6年（1721）に長者山三社堂（現在の新羅神社）に渡御したことが、八戸三社大祭の始まりと言われています。</p>
         <hr/>
+        <nav>
+        <Scroll to="concept"><Button>TOP</Button></Scroll>
+      </nav>
     </Layout>
   )
 }
